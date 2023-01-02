f2831c4345b4cf1bd6b309ba87aa40b47164b77c
Flutter Store <fluttertutorialstore@gmail.com>
2023-01-02 18:59:51 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const HeroApp());
+
+class HeroApp extends StatelessWidget {
+  const HeroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green
+      ),
+      home: HeroExample(),
+    );
+  }
+}
+
+class HeroExample extends StatelessWidget {
+  const HeroExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Hero',style: TextStyle(fontSize: 15),)
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(height: 20.0),
+          ListTile(
+            leading: const Hero(
+              tag: 'hero-rectangle',
+              child: BoxWidget(size: Size(50.0, 50.0)),
+            ),
+            onTap: () => _gotoDetailsPage(context),
+            title: const Text(
+              'Tap on the icon to view hero animation transition.',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _gotoDetailsPage(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) => Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Second Page',style: TextStyle(fontSize: 15),),
+        ),
+        body: const Center(
+          child: Hero(
+            tag: 'hero-rectangle',
+            child: BoxWidget(size: Size(200.0, 200.0)),
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class BoxWidget extends StatelessWidget {
+  const BoxWidget({super.key, required this.size});
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.green,
+    );
+  }
+}
